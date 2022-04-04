de1d3b9ea068cf364ca6fdb36cd1968546aa053f
Vijay Sekhri <sekhrivijay@google.com>
2022-04-04 21:39:11 +0000

    script file

---
+++ compile_batch_new.sh
@@ -0,0 +1,13 @@
+mvn compile exec:java \
+-Dexec.mainClass=com.google.cloud.teleport.templates.PubSubToBigQueryBatched \
+-Dexec.cleanupDaemonThreads=false \
+-Dexec.args=" \
+--streaming \
+--enableStreamingEngine \
+--project=${_PROJECT} \
+--region=us-central1 \
+--stagingLocation=${_BUCKET_NAME}/staging \
+--tempLocation=${_TEMP_LOCATION} \
+--templateLocation=${_TEMPLATE} \
+--useSubscription=true \
+--runner=DataflowRunner "
